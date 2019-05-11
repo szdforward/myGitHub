6b622168f8474e4a6f9972ed32e4f57f804ac7a8
forward22222 <szdforward@163.com>
2019-05-11 22:48:54 +0800

    remove directory from my repository

--- rpc-server/src/main/java/cn/ithome/rpc/server/RpcService.java
+++ rpc-server/src/main/java/cn/ithome/rpc/server/RpcService.java
@@ -7,7 +7,8 @@ import java.lang.annotation.Target;
 import org.springframework.stereotype.Component;
 
 /**
- * RPC 请求注解（标注在服务实现类上）
+ * 
+ * RPC 请求注解（标注在服务实现类上）   
  */
 @Target({ ElementType.TYPE })//注解用在接口上
 @Retention(RetentionPolicy.RUNTIME)//VM将在运行期也保留注释，因此可以通过反射机制读取注解的信息
